95106714423c56f2a32b52c5050c140c73b49b48
quali <qualicn@gmail.com>
2017-04-19 19:02:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import webapp2
  
 class ShowHome(webapp2.RequestHandler):
     def get(self):
+        temp_data = {}
+        temp_path = 'index.html'
+        self.response.out.write(template.render(temp_path,temp_data))
+      
 application = webapp2.WSGIApplication([
     ('/', ShowHome),
 ], debug=True)
